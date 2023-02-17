a2db555c10921ba8836a35df62cc3ed297286f70
thedanniel <116458078+thedanniel@users.noreply.github.com>
2023-02-17 14:29:14 -0300

    Add files via upload

---
+++ wemos/Funcoes.ino
@@ -0,0 +1,51 @@
+/*
+
+
+//OBS
+
+
+//Keyboard.write(KEY_F13);
+
+
+//PC
+void brilhoUp() {
+  Consumer.write(CONSUMER_BRIGHTNESS_UP);
+}
+void brilhoDown() {
+  Consumer.write(CONSUMER_BRIGHTNESS_DOWN);
+}
+void email() {
+  Consumer.write(CONSUMER_EMAIL_READER);
+}
+void navegador() {
+  Consumer.write(CONSUMER_BROWSER_HOME);
+}
+void explorer() {
+  Consumer.write(CONSUMER_EXPLORER);
+}
+void calculadora() {
+  Consumer.write(CONSUMER_CALCULATOR);
+}
+
+//SOM
+void prev() {
+  Consumer.write(MEDIA_PREV);
+}
+void next() {
+  Consumer.write(MEDIA_NEXT);
+}
+void playpause() {
+  Consumer.write(MEDIA_PLAY_PAUSE);
+}
+void mute() {
+  Consumer.write(MEDIA_VOLUME_MUTE);
+}
+void volUp() {
+  Consumer.write(MEDIA_VOLUME_UP);
+}
+void volDown() {
+  Consumer.write(MEDIA_VOL_DOWN);
+}
+
+
+*/
---
+++ wemos/Tela.ino
@@ -0,0 +1,90 @@
+//OBS
+
+void printOBS() {
+  tft.fillScreen(TFT_BLACK);
+  for (int i = 1; i < 13; i++) {
+    printTiles(i);
+  }
+  printREC();
+  printStopREC();
+  printLive();
+  printStopLive();
+  Gravacao();
+  Transmissao();
+  Controles();
+  Cena1();
+  Cena2();
+  Cena3();
+  Cena4();
+  printLogo();
+  printMic();
+  printSpeakerOBS();
+  backButton();
+}
+
+//Aplicativos
+
+void printAPPs() {
+  tft.fillScreen(TFT_BLACK);
+  printTiles(2);
+  printTiles(3);
+  printTiles(4);
+  printTiles(7);
+  printTiles(8);
+  printTaskManager();
+  printCalculadora();
+  printExplorer();
+  printEmail();
+  printInternet();
+  printTiles(9);  //back
+  backButton();
+}
+
+void printPC() {
+  tft.fillScreen(TFT_BLACK);
+  printTiles(6);
+  printTiles(7);
+  printTiles(8);
+  printSleep();
+  printPower();
+  printLock();
+  printTiles(9);  //back
+  backButton();
+}
+
+//SOM
+
+void printSOM() {
+  tft.fillScreen(TFT_BLACK);
+  printTiles(2);
+  printTiles(3);
+  printTiles(4);
+  printTiles(8);
+  printTiles(11);
+  printTiles(12);
+  printTiles(9);  //back
+  printPrev();
+  printPlayPause();
+  printNext();
+  printSpeakerUp();
+  printSpeakerDown();
+  printSpeakerMute();
+  backButton();  //retorno
+}
+
+void desenhaTriangulo(int x, int y, int w, int h, int o, uint32_t color) {
+  switch (o) {
+    case 0:
+      tft.fillTriangle(x, (y + (w / 2)), (x + h), y, (x + h), (y + w), color);
+      break;
+    case 1:
+      tft.fillTriangle(x, (y + h), (x + (w / 2)), y, (x + w), (y + h), color);
+      break;
+    case 2:
+      tft.fillTriangle(x, y, x, (y + w), (x + h), (y + (w / 2)), color);
+      break;
+    case 3:
+      tft.fillTriangle(x, y, (x + (w / 2)), (y + h), (x + w), y, color);
+      break;
+  }
+}
