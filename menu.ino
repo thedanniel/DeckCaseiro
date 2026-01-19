42aa804687e65ac766d402aeefee3372ef6e07c1
thedanniel <116458078+thedanniel@users.noreply.github.com>
2026-01-18 21:38:24 -0300

    Refactor setup function for improved initialization
    Initialize USB and controls earlier

--- menu.ino
+++ menu.ino
@@ -1,3 +1,5 @@
+//projeto em https://github.com/thedanniel/DeckCaseiro
+
 #include <SPI.h>
 #include <TFT_eSPI.h>
 #include <JPEGDecoder.h>
@@ -16,20 +18,23 @@ TFT_eSPI tft = TFT_eSPI();
 int tela = 0;
 
 void setup() {
+  USB.begin();
+  Keyboard.begin();
+  SystemControl.begin();
+  ConsumerControl.begin();
+
+  delay(1500);
+
   tft.begin();
   tft.setRotation(1);
   uint16_t calData[5] = { 197, 3488, 330, 3451, 1 };
   tft.setTouch(calData);
 
-  Serial.begin(115200);
+  delay(1500);
 
   sdBegin();
 
-  SystemControl.begin();
-  Keyboard.begin();
-  ConsumerControl.begin();
-  USB.begin();
-  MenuButtons();
+  renderTela(0);
 }
 
 void loop() {
