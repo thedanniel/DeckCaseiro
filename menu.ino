7bb9655454e11ee0eab1428a5b5ba42f9fc18a8c
thedanniel <116458078+thedanniel@users.noreply.github.com>
2026-01-20 12:18:23 -0300

    Implement deep sleep functionality in menu.ino
    Added support for deep sleep mode after inactivity.

--- menu.ino
+++ menu.ino
@@ -12,12 +12,22 @@ USBHIDConsumerControl ConsumerControl;
 USBHIDKeyboard Keyboard;
 #include "USBHIDSystemControl.h"
 USBHIDSystemControl SystemControl;
+#include "esp_sleep.h"
 
 TFT_eSPI tft = TFT_eSPI();
 
 int tela = 0;
 
+#define TOUCH_IRQ_PIN 18
+
+static uint32_t lastTouchMs = 0;
+static const uint32_t IDLE_SLEEP_MS = 60UL * 1000UL;
+
+
 void setup() {
+  pinMode(TOUCH_IRQ_PIN, INPUT_PULLUP);
+  lastTouchMs = millis();
+
   USB.begin();
   Keyboard.begin();
   SystemControl.begin();
@@ -35,8 +45,12 @@ void setup() {
   sdBegin();
 
   renderTela(0);
+  digitalWrite(TFT_BL, HIGH);
 }
 
 void loop() {
   toque();
+  if (millis() - lastTouchMs > IDLE_SLEEP_MS) {
+    entrarDeepSleepPorInatividade();
+  }
 }
